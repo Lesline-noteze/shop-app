198f37cf078344ec48c51d057f06ec48f009dc41
Lesline <leslinenoteze@gmail.com>
2026-07-01 13:07:55 +0100

    utilisation de signal et des pipes

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,27 +1,103 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 import { ProductItem } from '../product-item/product-item';
 import { product } from '../../models/product';
+import { CurrencyPipe, JsonPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductItem],
+  imports: [ProductItem, UpperCasePipe , JsonPipe, CurrencyPipe],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 
 export class ProductList {
-  products: product[] = [
-    { id: 1, name: 'produit 1', price: 25000, image: '/assets/products/img2.jpeg' },
-    { id: 2, name: 'robe rouge', price: 15000, image: '/assets/products/img1.jpeg' },
-    { id: 2, name: 'robe bleu', price: 15000, image: '/assets/products/im3.jpeg' },
-    { id: 1, name: 'robe traditionnelle', price: 25000, image: '/assets/products/img4.jpeg' },
-    { id: 2, name: 'robe bleu', price: 15000, image: '/assets/products/img5.jpeg' },
-    { id: 2, name: 'robe bleu', price: 15000, image: '/assets/products/img7.jpeg' },
-    { id: 2, name: 'robe bleu', price: 15000, image: '/assets/products/img9.jpeg' },
-    { id: 2, name: 'robe bleu', price: 15000, image: '/assets/products/img10.jpeg' },
-    { id: 2, name: 'robe bleu', price: 15000, image: '/assets/products/img11.jpeg' }
+
+  products= signal<product[]>([
+    { 
+      id:1,
+      name:'robe tra',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img1.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    },
+    { 
+      id:2,
+      name:'robe traditionnelle',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img2.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    },
+    { 
+      id:3,
+      name:'robe traditionnelle',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img3.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    },
+    { 
+      id:4,
+      name:'robe traditionnelle',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img4.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    },
+    { 
+      id:5,
+      name:'robe traditionnelle',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img5.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    },
+    { 
+      id:6,
+      name:'robe traditionnelle',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img6.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    },
+    { 
+      id:7,
+      name:'robe traditionnelle',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img7.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    },
+    { 
+      id:8,
+      name:'robe traditionnelle',
+      description:'Tissé à la main , motifs bamiléké',
+      soldeprice:22000,
+      regularprice:28000,
+      imageurl:'/assets/products/img8.jpeg',
+      createdAt: new Date('2026-01-15'),
+      categories:['vêtement', 'femme','traditionnel']
+    }
+
+    
+  ])
+  
 
 
 
-  ]
 }
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,6 +1,11 @@
 export interface product{
     id:number;
     name:string;
-    price:number;
-    image:string;
+    description: string;
+    soldeprice:number;
+    regularprice: number;
+    imageurl:string;
+    createdAt:Date;
+    updatedAt?: Date;
+    categories: string[];
 }
